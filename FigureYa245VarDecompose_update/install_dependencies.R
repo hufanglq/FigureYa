c3c6ff6e9f0ca7bad1c358d1fdb8320f39577d15
mypro <mypro@geyings-mac-pro.local>
2025-10-31 19:12:46 +0800

    add gene name

--- FigureYa245VarDecompose_update/install_dependencies.R
+++ FigureYa245VarDecompose_update/install_dependencies.R
@@ -50,7 +50,7 @@ cat("===========================================\n")
 
 # Installing CRAN packages
 cat("\nInstalling CRAN packages...\n")
-cran_packages <- c("doParallel", "lme4", "tidyverse")
+cran_packages <- c("doParallel", "lme4", "tidyverse", "ggrepel")
 
 for (pkg in cran_packages) {
   install_cran_package(pkg)
